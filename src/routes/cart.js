0fca68c86a5207de9cdf79306917bb641353b1b6
facuzero <facuzero_95@hotmail.com>
2022-03-03 18:08:14 -0300

    commit

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// ************ Require's ************
-const express = require('express');
-const router = express.Router();
-
-const {show,add} = require('../controllers/cartController');
-
-router.get('/show', show); 
-router.post('/:id',add)
-
-module.exports = router;
-
-
-=======
 const express = require('express');
 const router = express.Router();
 
@@ -23,4 +9,3 @@ router
     .post('/:id',add)
 
 module.exports = router;
->>>>>>> f054dced792a758fe68767fe79a5717cf8231466
